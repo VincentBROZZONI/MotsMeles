9a6613fcf3bcc24b3818c852ad3e4494d6066334
Vincent <toady73@hotmail.fr>
2019-09-11 09:18:29 +0200

    Première mis en commun

--- app/src/main/java/com/example/info706/View/MainActivity.java
+++ app/src/main/java/com/example/info706/View/MainActivity.java
@@ -1,13 +1,20 @@
-package com.example.info706;
+package com.example.info706.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.info706.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.listView = (ListView) findViewById(R.id.listViewMot);
+
     }
 }
