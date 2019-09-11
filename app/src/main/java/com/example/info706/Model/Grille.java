df069c573e4fc749c78c8c92e508c6d1c22b5645
Vincent <toady73@hotmail.fr>
2019-09-11 09:05:27 +0200

    Mis en place du modèle MVC + Début classes Mot et Grille

---
+++ app/src/main/java/com/example/info706/Model/Grille.java
@@ -0,0 +1,11 @@
+package com.example.info706.Model;
+
+import java.util.ArrayList;
+
+public class Grille {
+
+    private static final int TAILLE_DEFAULT = 10 ;
+    private ArrayList<Mot> listeMots;
+
+
+}
---
+++ app/src/main/java/com/example/info706/Model/Mot.java
@@ -0,0 +1,7 @@
+package com.example.info706.Model;
+
+public class Mot {
+
+    private int longueur_mot ;
+
+}
