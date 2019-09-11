9b0c732fed10c56e35d91f5c6eadd3d59a6fb173
jugand <jugandtheo@gmail.com>
2019-09-11 09:03:10 +0200

    Mise en place module(Création activié principale)

--- app/src/main/java/com/example/info706/MainActivity.java
+++ app/src/main/java/com/example/info706/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.info706;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewMot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.listViewMot = (ListView) findViewById(R.id.listViewMot);
     }
 }
